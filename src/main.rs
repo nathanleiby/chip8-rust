e171e7cb85548efbf8dd6f33816784adc4f972a6
Nathan Leiby <nathanleiby@gmail.com>
2024-11-23 18:54:39 -0800

    tweak how we draw screen
    Go back to blank vs "#" for the screen's off/on pixels.
    but add a border so boundary is clear

--- src/main.rs
+++ src/main.rs
@@ -370,10 +370,13 @@ impl Screen {
 
     fn draw(&self, pixels: Pixels) {
         println!("Screen:");
+        let margin_tb = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        println!("{}", margin_tb);
         for row in pixels.chunks(SCREEN_WIDTH) {
-            let s: String = row.iter().map(|x| if *x { "#" } else { "." }).collect();
-            println!("{}", s);
+            let s: String = row.iter().map(|x| if *x { "#" } else { " " }).collect();
+            println!("@{}@", s);
         }
+        println!("{}", margin_tb);
     }
 }
 const USAGE: &str = "usage: chip8-rust <file.ch8>";
